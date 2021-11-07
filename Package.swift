55a92ae15681218cf8f452ca34f013d41f3c43dc
Sascha Kempf <sascha.kempf@lasp.colorado.edu>
2021-11-06 18:20:00 -0600

    Fixed deprecated system package use.

--- Package.swift
+++ Package.swift
@@ -5,5 +5,13 @@ import PackageDescription
 let package = Package(
     name: "CHDF5",
     platforms: [ .macOS(.v10_12) ],
-    pkgConfig: "libhdf5"
+    products: [
+        .library(name: "CHDF5", type: nil, targets: ["CHDF5"])
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CHDF5",
+            pkgConfig: "libhdf5"
+        )
+    ]
 )
