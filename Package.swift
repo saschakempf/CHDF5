31189043cc52d91d64fe5a0259850d3f27e33189
Sascha Kempf <sascha.kempf@lasp.colorado.edu>
2021-11-06 18:07:33 -0600

    Added platform

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "CHDF5",
+    platforms: [ .macOS(.v10_12) ],
     pkgConfig: "libhdf5"
 )
